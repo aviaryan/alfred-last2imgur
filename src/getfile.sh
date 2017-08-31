5fee1c94bd669f60baaeff2bd41b906434eea0e6
Andy Huang <huanga@users.noreply.github.com>
2017-08-31 05:19:57 -0700

    Get correct path for screenshots (#3)
    * Make the getfile shell script pull the correct directory for screenshots.
    
    * Updated to detect when no `defaults` result, or simply not a valid directory.
    
    * We use `$DIR` not `$DIRECTORY` here.

--- src/getfile.sh
+++ src/getfile.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-USER=$(whoami)
-DIR="/Users/$USER/Desktop"
+DIR=$(defaults read com.apple.screencapture location)
+if [ ! -d "$DIR" ]; then
+	USER=$(whoami)
+	DIR="/Users/$USER/Desktop/"
+fi
+
 FILE=$(ls -t "$DIR" | head -n 1)
 
-echo "$DIR/$FILE"
+echo "$DIR$FILE"
