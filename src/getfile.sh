6af625f0ccd79b5e98d5da82ccbe84c79ec55bf1
i3p9 <sphincone@gmail.com>
2020-11-24 06:13:14 +0600

    Fix getfile.sh file path when DIR is taken from defaults

--- src/getfile.sh
+++ src/getfile.sh
@@ -3,9 +3,9 @@
 DIR=$(defaults read com.apple.screencapture location)
 if [ ! -d "$DIR" ]; then
 	USER=$(whoami)
-	DIR="/Users/$USER/Desktop/"
+	DIR="/Users/$USER/Desktop"
 fi
 
 FILE=$(ls -t "$DIR" | head -n 1)
 
-echo "$DIR$FILE"
+echo "$DIR/$FILE"
